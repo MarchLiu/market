738321eabd06978d44d2a5aed4a61138d83a6483
Mars Liu <mars.liu@outlook.com>
2019-01-07 00:00:21 +0800

    add symbol in query

--- messages/src/main/java/liu/mars/market/messages/NextOrder.java
+++ messages/src/main/java/liu/mars/market/messages/NextOrder.java
@@ -2,6 +2,7 @@ package liu.mars.market.messages;
 
 public class NextOrder {
     private long positionId;
+    private String symbol;
 
     public long getPositionId() {
         return positionId;
@@ -9,5 +10,13 @@ public class NextOrder {
 
     public void setPositionId(long positionId) {
         this.positionId = positionId;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
     }
 }
